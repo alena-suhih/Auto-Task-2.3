1d039311b6b8e4956ad618aac817b210d4e3f107
alena-suhih <prhalenka23@gmail.com>
2022-11-13 21:14:29 +0200

    Initial commit

---
+++ src/test/java/SubmitFormTests.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.*;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class SubmitFormTests {
+
+
+    @BeforeEach
+    void openBrowser() {open("http://localhost:9999");}
+
+    @Test
+    void shouldSuccessfulSubmitForm() {
+
+    }
+
+
+
+}

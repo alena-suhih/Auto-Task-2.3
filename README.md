0f17678024e49a862e7c1c781c61651645bdc812
alena-suhih <prhalenka23@gmail.com>
2022-11-13 22:55:04 +0200

    add .appveyor.yml, README.md, artifacts/app-card-delivery.jar, Tests

---
+++ README.md
@@ -0,0 +1,3 @@
+# Build status
+
+[![Build status](https://ci.appveyor.com/api/projects/status/68l4w3qqajjs0wo2?svg=true)](https://ci.appveyor.com/project/alena-suhih/auto-task-2-3)
--- src/test/java/SubmitFormTests.java
+++ src/test/java/SubmitFormTests.java
@@ -1,18 +1,38 @@
+import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.*;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Selenide.*;
 
 public class SubmitFormTests {
 
+    private String generateNeedDate(int plusDays, String pattern) {
+        return LocalDate.now().plusDays(plusDays).format(DateTimeFormatter.ofPattern(pattern));
+    }
 
     @BeforeEach
-    void openBrowser() {open("http://localhost:9999");}
-
-    @Test
-    void shouldSuccessfulSubmitForm() {
-
+    void openBrowser() {
+        open("http://localhost:9999");
     }
 
 
-
+    @Test
+    void shouldSuccessfulSubmitForm() {
+        $("[data-test-id=\"city\"] input").sendKeys("Саратов");
+        String currentDate = generateNeedDate(5, "dd.MM.yyyy");
+        $("[data-test-id=\"date\"] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME, Keys.DELETE));
+        $("[data-test-id=\"date\"] input").sendKeys(currentDate);
+        $("[data-test-id=\"name\"] input").sendKeys("Иванов-Петров Иван");
+        $("[data-test-id=\"phone\"] input").sendKeys("+79998877654");
+        $("[data-test-id=\"agreement\"] span").click();
+        $("button.button").click();
+        $("[data-test-id=\"notification\"]")
+                .shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldBe(Condition.text("Успешно!"))
+                .shouldBe(Condition.text("Встреча успешно забронирована на " + currentDate));
+    }
 }
